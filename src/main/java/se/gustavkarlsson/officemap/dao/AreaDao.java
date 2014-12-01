ed3d0d91d2251047b83d040e7b7792e05b401309
Gustav Karlsson <gustav.karlsson@gmail.com>
2014-12-01 23:33:31 +0100

    Updated AreaDao with Person Availability

--- src/main/java/se/gustavkarlsson/officemap/dao/AreaDao.java
+++ src/main/java/se/gustavkarlsson/officemap/dao/AreaDao.java
@@ -1,17 +1,52 @@
 package se.gustavkarlsson.officemap.dao;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.hibernate.SessionFactory;
 
 import se.gustavkarlsson.officemap.api.Reference;
 import se.gustavkarlsson.officemap.api.area.Area;
 import se.gustavkarlsson.officemap.api.area.AreaReference;
+import se.gustavkarlsson.officemap.api.person.Person;
+
+import com.google.common.base.Optional;
 
 public final class AreaDao extends AbstractItemDao<Area> {
 	
 	public AreaDao(final SessionFactory sessionFactory) {
 		super(sessionFactory);
+	}
+	
+	@Override
+	public Optional<Long> insert(final Area item) {
+		checkNotNull(item);
+		checkPersonAvailability(item);
+		return super.insert(item);
+	}
+	
+	@Override
+	public UpdateResponse update(final long referenceId, final Area item) {
+		checkNotNull(item);
+		checkPersonAvailability(item);
+		return super.update(referenceId, item);
+	}
+	
+	private void checkPersonAvailability(final Area item) {
+		final Set<Reference<Person>> persons = item.getPersons();
+		final List<Area> areas = findAllHeads();
+		areas.remove(item);
+		for (final Area area : areas) {
+			for (final Reference<Person> personRef : persons) {
+				if (area.getPersons().contains(personRef)) {
+					throw new IllegalStateException("Person with ID: " + personRef.getId()
+							+ " is already in area with ID: " + area.getId());
+				}
+			}
+		}
 	}
 	
 	@Override
--- src/test/java/se/gustavkarlsson/officemap/dao/AreaDaoTest.java
+++ src/test/java/se/gustavkarlsson/officemap/dao/AreaDaoTest.java
@@ -20,28 +20,28 @@ import se.gustavkarlsson.officemap.dao.ItemDao.UpdateResponse;
 import com.google.common.base.Optional;
 
 public class AreaDaoTest extends AbstractDaoTest {
-	
+
 	private static final Area validArea1 = Area.Builder.withFields(null, null,
 			new AreaReference(1l, Collections.<Area> emptyList()), false, "Floor 1", null).build();
 	private static final Area validArea2 = Area.Builder.withFields(1l, null, null, true, "Floor 2", null).build();
 	private static final Area validArea3 = Area.Builder.withFields(null, -400l, null, false, "Floor 3", null).build();
-	
+
 	private static final Person validPerson1 = Person.Builder.withFields(7l, null,
 			new PersonReference(1l, Collections.<Person> emptyList()), false, "gustavkarlsson", "Gustav", "Karlsson",
 			"gustav.karlsson@gmail.com").build();
 	private static final Person validPerson2 = Person.Builder.withFields(null, 500l,
 			new PersonReference(2l, Collections.<Person> emptyList()), false, "mickeymouse", "Mickey", "Mouse",
 			"mickey.mouse@disney.com").build();
-	
+
 	private static ItemDao<Area> dao;
 	private static ItemDao<Person> personDao;
-	
+
 	@BeforeClass
 	public static void initDao() {
 		dao = new AreaDao(getSessionFactory());
 		personDao = new PersonDao(getSessionFactory());
 	}
-	
+
 	@Test
 	public void insertValidAreasSucceeds() throws Exception {
 		final Area area1 = validArea1;
@@ -49,7 +49,7 @@ public class AreaDaoTest extends AbstractDaoTest {
 		dao.insert(area1);
 		dao.insert(area2);
 	}
-	
+
 	@Test
 	public void personEqualsAfterPersistence() throws Exception {
 		final Optional<Long> ref = dao.insert(validArea1);
@@ -58,7 +58,7 @@ public class AreaDaoTest extends AbstractDaoTest {
 		assertThat(fetchedArea.isPresent()).isTrue();
 		assertThat(fetchedArea.get()).isEqualTo(validArea1);
 	}
-	
+
 	@Test
 	public void refIncrementsAfterInsert() throws Exception {
 		final Optional<Long> ref1 = dao.insert(validArea1);
@@ -68,7 +68,7 @@ public class AreaDaoTest extends AbstractDaoTest {
 		assertThat(ref2.get()).isEqualTo(ref1Id + 1);
 		assertThat(ref3.get()).isEqualTo(ref1Id + 2);
 	}
-	
+
 	@Test
 	public void updateSetsHead() throws Exception {
 		final Optional<Long> ref = dao.insert(validArea1);
@@ -81,16 +81,16 @@ public class AreaDaoTest extends AbstractDaoTest {
 		assertThat(fetchedModifiedArea).isEqualTo(modifiedArea);
 		assertThat(fetchedModifiedArea.getId()).isEqualTo(2l);
 	}
-	
+
 	@Test
 	public void findAllHeads() throws Exception {
 		dao.insert(validArea1);
 		dao.insert(validArea2);
-		
+
 		final List<Area> heads = dao.findAllHeads();
 		assertThat(heads).containsExactly(validArea1, validArea2);
 	}
-	
+
 	@Test
 	public void findAllByReference() throws Exception {
 		dao.insert(validArea1);
@@ -98,22 +98,22 @@ public class AreaDaoTest extends AbstractDaoTest {
 		final Area area2Fetched = dao.findHeadByReference(area2Ref).get();
 		final Area area2Updated = Area.Builder.fromArea(area2Fetched).withName("Floor 1 modified").build();
 		dao.update(area2Updated.getReference().getId(), area2Updated);
-		
+
 		final List<Area> allWithArea2Ref = dao.findAllByReference(area2Ref);
 		assertThat(allWithArea2Ref).containsExactly(validArea2, area2Updated);
 	}
-
+	
 	@Test(expected = NullPointerException.class)
 	public void insertNullFails() throws Exception {
 		dao.insert(null);
 	}
-
+	
 	@Test(expected = ConstraintViolationException.class)
 	public void insertWithInvalidNameFails() throws Exception {
 		final Area area = Area.Builder.fromArea(validArea1).withName("").build();
 		dao.insert(area);
 	}
-	
+
 	@Test
 	public void timestampUpdatesOnInsert() throws Exception {
 		final long presetTimestamp = -1l;
@@ -122,7 +122,7 @@ public class AreaDaoTest extends AbstractDaoTest {
 		final Area fetchedArea = dao.findHeadByReference(reference.get()).get();
 		assertThat(fetchedArea.getTimestamp()).isNotEqualTo(presetTimestamp);
 	}
-
+	
 	@Test
 	public void referenceUpdatesOnInsert() throws Exception {
 		final Reference<Area> presetReference = new AreaReference();
@@ -130,7 +130,7 @@ public class AreaDaoTest extends AbstractDaoTest {
 		final Optional<Long> referenceId = dao.insert(area);
 		assertThat(referenceId.get()).isNotEqualTo(presetReference.getId());
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Test
 	public void validPersonsPersists() throws Exception {
@@ -138,31 +138,58 @@ public class AreaDaoTest extends AbstractDaoTest {
 		final Reference<Person> person1Ref = personDao.findHeadByReference(person1RefId).get().getReference();
 		final Long person2RefId = personDao.insert(validPerson1).get();
 		final Reference<Person> person2Ref = personDao.findHeadByReference(person2RefId).get().getReference();
-
+		
 		final Area area = Area.Builder.fromArea(validArea1).withPersonsVararg(person1Ref, person2Ref).build();
 		final Optional<Long> possibleArea1RefId = dao.insert(area);
 		final Optional<Area> fetchedArea = dao.findHeadByReference(possibleArea1RefId.get());
-
+		
 		assertThat(fetchedArea.get().getPersons()).containsOnly(person1Ref, person2Ref);
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Test
 	public void modifyingPersonsPersists() throws Exception {
 		final Long person1RefId = personDao.insert(validPerson1).get();
-		final Reference<Person> person1Ref = personDao.findHeadByReference(person1RefId).get().getReference();
+		final Reference<Person> person1Ref = personDao.findReference(person1RefId).get();
 		final Long person2RefId = personDao.insert(validPerson2).get();
-		final Reference<Person> person2Ref = personDao.findHeadByReference(person2RefId).get().getReference();
-
+		final Reference<Person> person2Ref = personDao.findReference(person2RefId).get();
+		
 		final Area area = Area.Builder.fromArea(validArea1).withPersonsVararg(person1Ref, person2Ref).build();
 		final Optional<Long> possibleArea1RefId = dao.insert(area);
 		final Optional<Area> fetchedArea = dao.findHeadByReference(possibleArea1RefId.get());
-
+		
 		final Area updatedArea = Area.Builder.fromArea(fetchedArea.get()).withPersonsVararg(person1Ref).build();
 		final UpdateResponse updateResponse = dao.update(possibleArea1RefId.get(), updatedArea);
 		assertThat(updateResponse).isSameAs(UpdateResponse.UPDATED);
 		final Optional<Area> updatedFetchedArea = dao.findHeadByReference(possibleArea1RefId.get());
-
+		
 		assertThat(updatedFetchedArea.get().getPersons()).containsOnly(person1Ref);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void insertOtherAreaWithPersonFails() throws Exception {
+		final Long person1RefId = personDao.insert(validPerson1).get();
+		final Reference<Person> personRef = personDao.findReference(person1RefId).get();
+		
+		final Area area = Area.Builder.fromArea(validArea1).withPersonsVararg(personRef).build();
+		dao.insert(area);
+		
+		final Area area2 = Area.Builder.fromArea(validArea1).withPersonsVararg(personRef).withName("Other Area")
+				.build();
+		dao.insert(area2);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void updateOtherAreaWithPersonFails() throws Exception {
+		final Long person1RefId = personDao.insert(validPerson1).get();
+		final Reference<Person> personRef = personDao.findReference(person1RefId).get();
+		
+		final Area area = Area.Builder.fromArea(validArea1).withPersonsVararg(personRef).build();
+		dao.insert(area);
+		
+		final Area area2 = Area.Builder.fromArea(validArea1).withName("Other Area").build();
+		dao.insert(area2);
+		final Area area2Updated = Area.Builder.fromArea(area2).withPersonsVararg(personRef).build();
+		dao.update(area2Updated.getReference().getId(), area2Updated);
 	}
 }
