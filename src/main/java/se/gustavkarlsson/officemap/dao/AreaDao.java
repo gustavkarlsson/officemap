52070750965734999753c1dd222830ff361def02
Gustav Karlsson <gustav.karlsson@gmail.com>
2014-12-02 20:37:15 +0100

    Fixed area update check

--- src/main/java/se/gustavkarlsson/officemap/dao/AreaDao.java
+++ src/main/java/se/gustavkarlsson/officemap/dao/AreaDao.java
@@ -16,62 +16,63 @@ import se.gustavkarlsson.officemap.api.person.Person;
 import com.google.common.base.Optional;
 
 public final class AreaDao extends AbstractItemDao<Area> {
-	
+
 	public AreaDao(final SessionFactory sessionFactory) {
 		super(sessionFactory);
 	}
-	
+
 	@Override
 	public Optional<Long> insert(final Area item) {
 		checkNotNull(item);
 		checkPersonAvailability(item);
 		return super.insert(item);
 	}
-	
+
 	@Override
 	public UpdateResponse update(final long referenceId, final Area item) {
 		checkNotNull(item);
 		checkPersonAvailability(item);
 		return super.update(referenceId, item);
 	}
-	
+
 	private void checkPersonAvailability(final Area item) {
 		final Set<Reference<Person>> persons = item.getPersons();
 		final List<Area> areas = findAllHeads();
-		areas.remove(item);
 		for (final Area area : areas) {
-			for (final Reference<Person> personRef : persons) {
-				if (area.getPersons().contains(personRef)) {
-					throw new IllegalStateException("Person with ID: " + personRef.getId()
-							+ " is already in area with ID: " + area.getId());
+			if (!area.getId().equals(item.getId())) {
+				for (final Reference<Person> personRef : persons) {
+					if (area.getPersons().contains(personRef)) {
+						throw new IllegalStateException("Person with ID: " + personRef.getId()
+								+ " is already in area with ID: " + area.getId());
+					}
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	protected Area setId(final Area item, final Long id) {
 		final Area prepared = Area.Builder.fromArea(item).withId(id).build();
 		return prepared;
 	}
-	
+
 	@Override
 	protected Area setReference(final Area item, final Reference<Area> reference) {
 		final Area prepared = Area.Builder.fromArea(item).withReference(reference).build();
 		return prepared;
 	}
-	
+
 	@Override
 	protected Area setTimestamp(final Area item, final Long timestamp) {
 		final Area prepared = Area.Builder.fromArea(item).withTimestamp(timestamp).build();
 		return prepared;
 	}
-	
+
 	@Override
 	protected Reference<Area> createNewReference() {
 		return new AreaReference(null, new ArrayList<Area>());
 	}
-	
+
 	@Override
 	protected Class<? extends Reference<Area>> getReferenceClass() {
 		return AreaReference.class;
--- src/test/java/se/gustavkarlsson/officemap/api/AreaTest.java
+++ src/test/java/se/gustavkarlsson/officemap/api/AreaTest.java
@@ -17,90 +17,90 @@ import se.gustavkarlsson.officemap.test.TestAreaBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AreaTest {
-	
-	private static final ObjectMapper mapper = Jackson.newObjectMapper();
-	
-	private static final Area area = TestAreaBuilder.withTestParameters().build();
 
-	private static String fixture;
+	private static final ObjectMapper mapper = Jackson.newObjectMapper();
+
+	private static final Area area = TestAreaBuilder.withTestParameters().build();
 	
+	private static String fixture;
+
 	@BeforeClass
 	public static void setUp() throws Exception {
 		fixture = fixture("fixtures/area.json");
 	}
-	
+
 	@Test
 	public void serializesToJSON() throws Exception {
 		final String serialized = mapper.writeValueAsString(area);
 		assertEquals(fixture, serialized, true);
 	}
-	
+
 	@Test
 	public void deserializesFromJSON() throws Exception {
 		final Area deserialized = mapper.readValue(fixture, Area.class);
 		assertThat(deserialized).isEqualTo(area);
 	}
-	
+
 	@Test
 	public void validAreaValidates() throws Exception {
 		assertThat(area).isValid();
 	}
-	
+
 	@Test
 	public void invalidId() throws Exception {
 		assertInvalidId(-1l);
 		assertInvalidId(Long.MIN_VALUE);
 	}
-	
+
 	@Test
 	public void invalidTimestamp() throws Exception {
 		assertInvalidTimestamp(-1l);
 		assertInvalidTimestamp(Long.MIN_VALUE);
 	}
-	
+
 	@Test
 	public void invalidName() throws Exception {
 		assertInvalidName(null);
 		assertInvalidName("");
 		assertInvalidName(" \t\n");
 	}
-	
+
 	@Test
 	public void nullPersonsCreatesEmptyPersons() throws Exception {
 		final Area areaToTest = Area.Builder.fromArea(area).withPersons(null).build();
 		assertThat(areaToTest).isValid();
 	}
-	
+
 	@Test
 	public void nullPersonsAreRemoved() throws Exception {
 		final Reference<Person> nullPerson = null;
 		final Area areaToTest = Area.Builder.fromArea(area).withPersonsVararg(nullPerson).build();
 		org.fest.assertions.api.Assertions.assertThat(areaToTest.getPersons()).isEmpty();
 	}
-	
+
 	@Test
 	public void equalsContract() throws Exception {
 		EqualsVerifier.forClass(Area.class).usingGetClass().allFieldsShouldBeUsedExcept("id", "timestamp", "reference")
 				.verify();
 	}
-	
+
 	private void assertInvalidId(final Long id) {
 		final Area invalidArea = Builder.fromArea(area).withId(id).build();
 		assertThatAreaHasInvalid(invalidArea, "id");
 	}
-	
+
 	private void assertInvalidTimestamp(final Long timestamp) {
 		final Area invalidArea = Builder.fromArea(area).withTimestamp(timestamp).build();
 		assertThatAreaHasInvalid(invalidArea, "timestamp");
 	}
-	
+
 	private void assertInvalidName(final String username) {
 		final Area invalidArea = Builder.fromArea(area).withName(username).build();
 		assertThatAreaHasInvalid(invalidArea, "name");
 	}
-	
+
 	private void assertThatAreaHasInvalid(final Area area, final String property) {
 		assertThat(area).hasInvalid(property);
 	}
-	
+
 }
