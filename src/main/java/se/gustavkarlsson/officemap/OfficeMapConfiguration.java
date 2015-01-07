837d944fed86c8f8958a23972a5ec479946f71bc
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-01-08 00:50:39 +0100

    Removed superflous temp dir configuration field.

--- src/main/java/se/gustavkarlsson/officemap/OfficeMapConfiguration.java
+++ src/main/java/se/gustavkarlsson/officemap/OfficeMapConfiguration.java
@@ -14,40 +14,28 @@ import org.hibernate.validator.constraints.NotBlank;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class OfficeMapConfiguration extends Configuration {
-
+	
 	@NotBlank
 	@JsonProperty
 	private String dataPath = "data/";
 
-	@NotBlank
-	@JsonProperty
-	private String tempPath = "temp/";
-	
 	@Valid
 	@NotNull
 	@JsonProperty
 	private final DataSourceFactory database = createDefaultDataSourceFactory();
-
+	
 	public final Path getDataPath() {
 		return Paths.get(dataPath);
 	}
-	
+
 	public final void setDataPath(final String dataPath) {
 		this.dataPath = dataPath;
 	}
-	
-	public final Path getTempPath() {
-		return Paths.get(tempPath);
-	}
-	
-	public final void setTempPath(final String tempPath) {
-		this.tempPath = tempPath;
-	}
-	
+
 	public final DataSourceFactory getDataSourceFactory() {
 		return database;
 	}
-	
+
 	private DataSourceFactory createDefaultDataSourceFactory() {
 		final DataSourceFactory factory = new DataSourceFactory();
 		factory.setDriverClass("org.h2.Driver");
