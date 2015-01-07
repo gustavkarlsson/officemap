aa4fecb2bd814354dc002e860d7b58834621f688
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-01-07 23:01:53 +0100

    Made file transfers nicer.

--- src/main/java/se/gustavkarlsson/officemap/util/FileHandler.java
+++ src/main/java/se/gustavkarlsson/officemap/util/FileHandler.java
@@ -23,15 +23,15 @@ import com.google.common.net.MediaType;
 
 public class FileHandler {
 	private static final Logger logger = LoggerFactory.getLogger(FileHandler.class);
-	
-	private static final int BUFFER_SIZE = 1024;
+
+	private static final int BUFFER_SIZE = 4096;
 	private static final int NO_BYTES_READ = -1;
-	
+
 	private final Tika tika = new Tika();
 
 	private final MessageDigest sha1Digest;
 	private final Path dataPath;
-	
+
 	public FileHandler(final Path dataPath) {
 		try {
 			this.sha1Digest = MessageDigest.getInstance("SHA-1");
@@ -41,16 +41,16 @@ public class FileHandler {
 		this.dataPath = checkNotNull(dataPath);
 		logger.debug("dataPath: " + dataPath);
 	}
-	
+
 	public Sha1 writeFile(final InputStream input) {
 		checkNotNull(input);
 		logger.debug("Saving file");
 		try {
 			final Path tempFile = Files.createTempFile(null, null);
 			logger.debug("Temp file: " + tempFile);
-			
+
 			logger.debug("Transferring...");
-			final Sha1 fileSha1 = transferData(input, Files.newOutputStream(tempFile));
+			final Sha1 fileSha1 = transferData(input, Files.newOutputStream(tempFile), true);
 			final Path targetFile = dataPath.resolve(fileSha1.getHex());
 			logger.debug("Target file: " + targetFile);
 			try {
@@ -64,20 +64,30 @@ public class FileHandler {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	// TODO use NIO? (http://stackoverflow.com/questions/16050827/filechannel-bytebuffer-and-hashing-files)
-	private Sha1 transferData(final InputStream inputStream, final OutputStream output) throws IOException {
-		int read = 0;
-		final byte[] buffer = new byte[BUFFER_SIZE];
-		while ((read = inputStream.read(buffer)) != NO_BYTES_READ) {
-			output.write(buffer, 0, read);
-			sha1Digest.update(buffer, 0, read);
+
+	private Sha1 transferData(final InputStream input, final OutputStream output, final boolean calculateSha1)
+			throws IOException {
+		try {
+			int read = 0;
+			final byte[] buffer = new byte[BUFFER_SIZE];
+			while ((read = input.read(buffer)) != NO_BYTES_READ) {
+				output.write(buffer, 0, read);
+				if (calculateSha1) {
+					sha1Digest.update(buffer, 0, read);
+				}
+			}
+			output.flush();
+			if (calculateSha1) {
+				final byte[] sha1Bytes = sha1Digest.digest();
+				return Sha1.builder().withBytes(sha1Bytes).build();
+			}
+			return null;
+		} finally {
+			input.close();
+			output.close();
 		}
-		output.flush();
-		final byte[] sha1 = sha1Digest.digest();
-		return Sha1.builder().withBytes(sha1).build();
 	}
-	
+
 	public boolean exists(final Sha1 fileSha1) {
 		checkNotNull(fileSha1);
 		logger.debug("Getting file: " + fileSha1.getHex());
@@ -92,13 +102,13 @@ public class FileHandler {
 		}
 		return true;
 	}
-	
+
 	public StreamingOutput readFile(final Sha1 fileSha1) {
 		logger.debug("Getting stream of file: " + fileSha1.getHex());
 		final Path path = dataPath.resolve(fileSha1.getHex());
 		return new FileStreamingOutput(path);
 	}
-	
+
 	public String getMimeType(final Sha1 fileSha1) {
 		checkNotNull(fileSha1);
 		final Path path = dataPath.resolve(fileSha1.getHex());
@@ -113,26 +123,19 @@ public class FileHandler {
 		logger.debug("MIME type is " + mimeType);
 		return mimeType;
 	}
-	
-	private static class FileStreamingOutput implements StreamingOutput {
-		
+
+	private class FileStreamingOutput implements StreamingOutput {
+
 		private final Path path;
-		
+
 		public FileStreamingOutput(final Path path) {
 			this.path = checkNotNull(path);
 		}
-		
+
 		// TODO use NIO? (http://stackoverflow.com/questions/16050827/filechannel-bytebuffer-and-hashing-files)
 		@Override
 		public void write(final OutputStream output) throws IOException {
-			try (InputStream input = Files.newInputStream(path)) {
-				final byte[] buffer = new byte[BUFFER_SIZE];
-				int read = 0;
-				while ((read = input.read(buffer)) != NO_BYTES_READ) {
-					output.write(buffer, 0, read);
-				}
-				output.flush();
-			}
+			transferData(Files.newInputStream(path), output, false);
 		}
 	}
 }
