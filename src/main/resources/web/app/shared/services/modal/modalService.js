4e3d58aaa6f464f0088ab0874e0556398d77bb8f
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-01-31 20:55:54 +0100

    Added modal service

---
+++ src/main/resources/web/app/shared/services/modal/modalService.js
@@ -0,0 +1,65 @@
+/*global alert */
+/*global L */
+/*global angular */
+/*jshint -W089 */
+
+(function () {
+	"use strict";
+	var app = angular.module("main");
+
+	app.factory("ModalService", function ($modal) {
+		var modalDefaults, modalOptions, show;
+		
+		modalDefaults = {
+			templateUrl: "/app/shared/services/modal/modalView.html"
+		};
+
+		modalOptions = {
+			confirmButtonText: "OK",
+			cancelButtonText: "Cancel",
+			confirmButtonType: "primary",
+			cancelButtonType: "link",
+			headerText: "Are you sure?"
+		};
+		
+		show = function (customModalDefaults, customModalOptions) {
+			var tempModalDefaults = {}, tempModalOptions = {};
+			
+			angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+			angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+			if (!tempModalDefaults.controller) {
+				tempModalDefaults.controller = function ($scope, $modalInstance) {
+					$scope.modalOptions = tempModalOptions;
+					$scope.modalOptions.confirm = function (result) {
+						$modalInstance.close(result);
+					};
+					$scope.modalOptions.cancel = function (result) {
+						$modalInstance.dismiss("cancel");
+					};
+				};
+			}
+
+			return $modal.open(tempModalDefaults).result;
+		};
+		
+		return {
+			showModal: function (customModalDefaults, customModalOptions) {
+				//if (!customModalDefaults) {
+				//	customModalDefaults = {};
+				//}
+				return show(customModalDefaults, customModalOptions);
+			},
+			showDeleteModal: function (customModalDefaults, customModalOptions) {
+				if (!customModalOptions) {
+					customModalOptions = {};
+				}
+				customModalOptions.confirmButtonText = "Delete";
+				customModalOptions.confirmButtonType = "danger";
+				customModalOptions.headerText = "Delete?";
+				return show(customModalDefaults, customModalOptions);
+			}
+		};
+	});
+
+}());
