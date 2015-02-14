9cc27df5a1fc699f5140e280df1a150fb9cdfbf7
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-02-14 13:38:54 +0100

    Servicified DiffService

--- src/main/resources/web/app/shared/services/diffService.js
+++ src/main/resources/web/app/shared/services/diffService.js
@@ -7,21 +7,20 @@
 	"use strict";
 	var app = angular.module("main");
 
-	app.factory("DiffService", function () {
-		return {
-			getChanges: function (original, changed) {
-				var changes = {},
-					prop;
-				for (prop in changed) {
-					if (changed.hasOwnProperty(prop) && original.hasOwnProperty(prop)) {
-						if (!angular.equals(original[prop], changed[prop])) {
-							changes[prop] = changed[prop];
-						}
-					}
-				}
-				return changes;
-			}
-		};
+	app.service("DiffService", function () {
+
+		this.getChanges = function (original, changed) {
+            var changes = {},
+                prop;
+            for (prop in changed) {
+                if (changed.hasOwnProperty(prop) && original.hasOwnProperty(prop)) {
+                    if (!angular.equals(original[prop], changed[prop])) {
+                        changes[prop] = changed[prop];
+                    }
+                }
+            }
+            return changes;
+        };
 	});
 
 }());
