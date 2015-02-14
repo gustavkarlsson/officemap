10099d3f4fe37b1c7ecc1167e47461aff5878dae
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-02-14 13:45:26 +0100

    Servicialized MapService

--- src/main/resources/web/app/shared/services/mapService.js
+++ src/main/resources/web/app/shared/services/mapService.js
@@ -8,68 +8,70 @@
 
 	app.factory("MapService", function ($http, $q) {
 
-		return {
-			get: function (ref) {
-				var deferred = $q.defer();
-				$http.get("/api/maps/" + ref)
-					.success(function (data, status) {
-						deferred.resolve(data);
-					})
-					.error(function (data, status) {
-						deferred.reject(data);
-					});
+        this.get = function (ref) {
+            var deferred = $q.defer();
+            $http.get("/api/maps/" + ref)
+                .success(function (data, status) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status) {
+                    deferred.reject(data);
+                });
 
-				return deferred.promise;
-			},
-			getAll: function () {
-				var deferred = $q.defer();
-				$http.get("/api/maps/")
-					.success(function (data, status) {
-						deferred.resolve(data);
-					})
-					.error(function (data, status) {
-						deferred.reject(data);
-					});
+            return deferred.promise;
+        };
 
-				return deferred.promise;
-			},
-			create: function (map) {
-				var deferred = $q.defer();
-				$http.post("/api/maps/", map)
-					.success(function (ref) {
-						deferred.resolve(ref);
-					})
-					.error(function (data, status) {
-						deferred.reject(data);
-					});
+        this.getAll = function () {
+            var deferred = $q.defer();
+            $http.get("/api/maps/")
+                .success(function (data, status) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status) {
+                    deferred.reject(data);
+                });
 
-				return deferred.promise;
-			},
-			update: function (ref, changes) {
-				var deferred = $q.defer();
-				$http.patch("/api/maps/" + ref, changes)
-					.success(function (data, status) {
-						deferred.resolve(data);
-					})
-					.error(function (data, status) {
-						deferred.reject(data);
-					});
+            return deferred.promise;
+        };
 
-				return deferred.promise;
-			},
-			remove: function (ref) {
-				var deferred = $q.defer();
-				$http["delete"]("/api/maps/" + ref)
-					.success(function (data, status) {
-						deferred.resolve(data);
-					})
-					.error(function (data, status) {
-						deferred.reject(data);
-					});
+        this.create = function (map) {
+            var deferred = $q.defer();
+            $http.post("/api/maps/", map)
+                .success(function (ref) {
+                    deferred.resolve(ref);
+                })
+                .error(function (data, status) {
+                    deferred.reject(data);
+                });
 
-				return deferred.promise;
-			}
-		};
+            return deferred.promise;
+        };
+
+        this.update = function (ref, changes) {
+            var deferred = $q.defer();
+            $http.patch("/api/maps/" + ref, changes)
+                .success(function (data, status) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status) {
+                    deferred.reject(data);
+                });
+
+            return deferred.promise;
+        };
+
+        this.remove = function (ref) {
+            var deferred = $q.defer();
+            $http["delete"]("/api/maps/" + ref)
+                .success(function (data, status) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status) {
+                    deferred.reject(data);
+                });
+
+            return deferred.promise;
+        };
 
 	});
 
