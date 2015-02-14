7d79ac03268c58e8902672e88bf35f3c3ff72b39
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-02-14 13:42:20 +0100

    Servicified ImageService

--- src/main/resources/web/app/shared/services/imageService.js
+++ src/main/resources/web/app/shared/services/imageService.js
@@ -7,43 +7,41 @@
 	"use strict";
 	var app = angular.module("main");
 
-	app.factory("ImageService", function ($q, $http) {
+	app.service("ImageService", function ($q, $http) {
 
-		// Methods
-		return {
-			get: function (sha1) {
-				var deferred, image;
-				deferred = $q.defer();
-				image = new Image();
+        this.get = function (sha1) {
+            var deferred, image;
+            deferred = $q.defer();
+            image = new Image();
 
-				image.onload = function () {
-					deferred.resolve(this);
-				};
-				image.error = function () {
-					deferred.reject(false);
-				};
+            image.onload = function () {
+                deferred.resolve(this);
+            };
+            image.error = function () {
+                deferred.reject(false);
+            };
 
-				image.src = "/api/files/" + sha1;
-				return deferred.promise;
-			},
-			upload: function (file) {
-				var deferred = $q.defer(),
-					formData = new FormData();
-				formData.append("file", file);
-				$http.post("/api/files/", formData, {
-					transformRequest: angular.identity,
-					headers: {
-						"Content-Type": undefined
-					}
-				}).success(function (sha1) {
-					deferred.resolve(sha1);
-				}).error(function (data, status) {
-					deferred.reject(data);
-				});
+            image.src = "/api/files/" + sha1;
+            return deferred.promise;
+        };
 
-				return deferred.promise;
-			}
-		};
+		this.upload = function (file) {
+            var deferred = $q.defer(),
+                formData = new FormData();
+            formData.append("file", file);
+            $http.post("/api/files/", formData, {
+                transformRequest: angular.identity,
+                headers: {
+                    "Content-Type": undefined
+                }
+            }).success(function (sha1) {
+                deferred.resolve(sha1);
+            }).error(function (data, status) {
+                deferred.reject(data);
+            });
+
+            return deferred.promise;
+        };
 
 	});
 
