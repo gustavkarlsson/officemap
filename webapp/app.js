80cf96c62e38dceb0f03d1ae3e1afc58bb78cbd8
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-07-01 23:35:13 +0200

    Added URL rewriting to webapp

--- webapp/app.js
+++ webapp/app.js
@@ -2,17 +2,29 @@ var express = require('express'),
     compression = require('compression'),
     request = require('request');
 
-var oneDay = 86400000,
+var cacheDuration = 86400000, // One day
     apiUrl = 'http://localhost:8080',
-    staticDir = '/public';
+    staticDir = '/public/',
+    indexFile = 'index.html';
 
 var app = express();
 
+// Use Gzip compression
 app.use(compression());
-app.use(express.static(__dirname + staticDir, { maxAge: oneDay }));
+
+// Serve static content and use cache
+app.use(express.static(__dirname + staticDir, { maxAge: cacheDuration }));
+
+// Proxy for backend calls
 app.use('/api', function(req, res) {
   var url = apiUrl + req.url;
   req.pipe(request(url)).pipe(res);
 });
 
+// URL rewriting
+app.get('*', function(request, response, next) {
+  response.sendfile(__dirname + staticDir + indexFile);
+});
+
+// Start server
 app.listen(process.env.PORT || 3000);
