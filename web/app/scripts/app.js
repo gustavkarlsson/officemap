d2db0aeb3db9c57f1f15fa4fa3f7c6c51d7a8773
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-07-03 20:00:58 +0200

    Restructured project with yeoman

---
+++ web/app/scripts/app.js
@@ -0,0 +1,151 @@
+/*global angular */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name officemapWebappApp
+ * @description
+ * # officemapWebappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('main', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'leaflet-directive',
+    'angularFileUpload'
+  ])
+  .config(function ($locationProvider) {
+      $locationProvider.html5Mode(true);
+  })
+  .config(function ($routeProvider) {
+      $routeProvider
+          .when("/", {
+              templateUrl: "/app/components/home/homeView.html"
+          })
+          .when("/persons/:ref", {
+              templateUrl: "/app/components/map/mapView.html",
+              controller: "MapController",
+              resolve: {
+                  mapRef: function (PersonService, $route) {
+                      return PersonService.get($route.current.params.ref).then(function (person) {
+                          return person.location.mapRef;
+                      });
+                  },
+                  map: function (PersonService, MapService, $route) {
+                      return PersonService.get($route.current.params.ref).then(function (person) {
+                          return MapService.get(person.location.mapRef);
+                      });
+                  },
+                  image: function (PersonService, MapService, ImageService, $route) {
+                      return PersonService.get($route.current.params.ref).then(function (person) {
+                          return MapService.get(person.location.mapRef).then(function (map) {
+                              return ImageService.get(map.image);
+                          });
+                      });
+                  },
+                  persons: function (PersonService, $route) {
+                      return PersonService.get($route.current.params.ref).then(function (person) {
+                          return PersonService.getAllByMapRef(person.location.mapRef);
+                      });
+                  },
+                  activePersonRef: function ($route) {
+                      return $route.current.params.ref;
+                  }
+              }
+          })
+          .when("/maps/:ref", {
+              templateUrl: "/app/components/map/mapView.html",
+              controller: "MapController",
+              resolve: {
+                  mapRef: function ($route) {
+                      return $route.current.params.ref;
+                  },
+                  map: function (MapService, $route) {
+                      return MapService.get($route.current.params.ref);
+                  },
+                  image: function (MapService, ImageService, $route) {
+                      return MapService.get($route.current.params.ref).then(function (map) {
+                          return ImageService.get(map.image);
+                      });
+                  },
+                  persons: function (PersonService, $route) {
+                      return PersonService.getAllByMapRef($route.current.params.ref);
+                  },
+                  activePersonRef: function () {
+                      return null;
+                  }
+              }
+          })
+          .when("/admin", {
+              templateUrl: "/app/components/admin/adminView.html",
+              controller: "AdminController",
+              resolve: {
+                  persons: function (PersonService) {
+                      return PersonService.getAll();
+                  },
+                  maps: function (MapService) {
+                      return MapService.getAll();
+                  }
+              }
+          })
+          .when("/admin/maps/new", {
+              templateUrl: "/app/components/editMap/editMapView.html",
+              controller: "EditMapController",
+              resolve: {
+                  ref: function ($route) {
+                      return null;
+                  },
+                  map: function (MapService, $route) {
+                      return null;
+                  }
+              }
+          })
+          .when("/admin/maps/:ref", {
+              templateUrl: "/app/components/editMap/editMapView.html",
+              controller: "EditMapController",
+              resolve: {
+                  ref: function ($route) {
+                      return $route.current.params.ref;
+                  },
+                  map: function (MapService, $route) {
+                      return MapService.get($route.current.params.ref);
+                  }
+              }
+          })
+          .when("/admin/persons/new", {
+              templateUrl: "/app/components/editPerson/editPersonView.html",
+              controller: "EditPersonController",
+              resolve: {
+                  ref: function ($route) {
+                      return null;
+                  },
+                  person: function (PersonService, $route) {
+                      return null;
+                  }
+              }
+          })
+          .when("/admin/persons/:ref", {
+              templateUrl: "/app/components/editPerson/editPersonView.html",
+              controller: "EditPersonController",
+              resolve: {
+                  ref: function ($route) {
+                      return $route.current.params.ref;
+                  },
+                  person: function (PersonService, $route) {
+                      return PersonService.get($route.current.params.ref);
+                  }
+              }
+          })
+          .otherwise({
+              redirectTo: "/"
+          });
+  });
