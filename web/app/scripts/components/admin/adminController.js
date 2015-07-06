386a28b33bac069faa7d9c7f42640db92f1afcb7
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-07-06 22:06:01 +0200

    Materialized admin view

--- web/app/scripts/components/admin/adminController.js
+++ web/app/scripts/components/admin/adminController.js
@@ -4,9 +4,14 @@
 	"use strict";
 	var app = angular.module("main");
 
-	app.controller("AdminController", function($scope, persons, maps) {
+	app.controller("AdminController", function($scope, $location, persons, maps) {
 		$scope.persons = persons;
 		$scope.maps = maps;
+
+    $scope.go = function(path) {
+      $location.path(path);
+    };
+
 	});
 
 }());
