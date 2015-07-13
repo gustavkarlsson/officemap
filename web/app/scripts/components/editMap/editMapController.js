fcc9a57f92f0ecc9f90192c0146176df325da11a
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-07-13 20:23:13 +0200

    Cleaned up a lot in editMap

--- web/app/scripts/components/editMap/editMapController.js
+++ web/app/scripts/components/editMap/editMapController.js
@@ -1,95 +1,88 @@
 /* global alert */
 /* global angular */
 
-(function() {
-	"use strict";
-	var app = angular.module("main");
+(function () {
+  "use strict";
+  var app = angular.module("main");
 
-	app.controller("EditMapController", function($scope, $location, $route, ref, map, MapService, DiffService,
-		ImageService, ModalService) {
-		var originalMap = angular.copy(map);
+  app.controller("EditMapController", function ($scope, $state, ref, map, MapService, DiffService, ImageService) {
+    // Variables
+    var originalMap;
 
-		$scope.ref = ref;
-		$scope.map = map;
-		if ($scope.map === null) {
-			$scope.map = {};
-		}
+    // Static
+    originalMap = map ? angular.copy(map) : {};
 
-		$scope.isChanged = function() {
-			return !angular.equals($scope.map, originalMap);
-		};
+    // Scope
+    $scope.ref = ref;
+    $scope.map = angular.copy(originalMap);
+    $scope.isNew = originalMap === {}
 
-		$scope.revert = function() {
-			$scope.map = angular.copy(originalMap);
-		};
+    $scope.isChanged = function () {
+      return !angular.equals($scope.map, originalMap);
+    };
 
-		$scope.isNew = function() {
-			return originalMap === null;
-		};
+    $scope.revert = function () {
+      $scope.map = angular.copy(originalMap);
+    };
 
-		$scope.create = function() {
-			var promise = MapService.create($scope.map);
-			promise.then(function(ref) {
-				$location.path("/admin");
-				//TODO toast success
-			}, function(reason) {
-				alert("Failed: " + reason);
-				//TODO toast failed
-			});
-		};
+    $scope.create = function () {
+      MapService.create($scope.map)
+        .then(function (ref) {
+          $state.go("admin.home", { tab: "maps" });
+          //TODO toast success
+        }, function (reason) {
+          alert("Failed: " + reason);
+          //TODO toast failed
+        });
+    };
 
-		$scope.update = function() {
-			var changes = DiffService.getChanges(originalMap, $scope.map),
-				promise;
-			promise = MapService.update($scope.ref, changes);
-			promise.then(function() {
-				$location.path("/admin");
-				//TODO toast success
-			}, function(reason) {
-				alert("Failed: " + reason);
-				//TODO toast failed
-			});
-		};
+    $scope.update = function () {
+      var changes = DiffService.getChanges(originalMap, $scope.map);
+      MapService.update($scope.ref, changes)
+        .then(function () {
+          $state.go("admin.home", { tab: "maps" });
+          //TODO toast success
+        }, function (reason) {
+          alert("Failed: " + reason);
+          //TODO toast failed
+        });
+    };
 
-		$scope.remove = function() {
-			var modalOptions = {
-				bodyText: "Are you sure you want to delete this map?"
-			};
+    $scope.remove = function () {
+      // TODO add modal with confirmation
+      MapService.remove($scope.ref)
+        .then(function () {
+          $state.go("admin.home", { tab: "maps" });
+          //TODO toast success
+        }, function (reason) {
+          alert("Failed: " + reason);
+          //TODO toast failed
+        });
+    };
 
-			ModalService.showDeleteModal({}, modalOptions).then(function(result) {
-				var promise = MapService.remove($scope.ref);
-				promise.then(function() {
-					$location.path("/admin");
-					//TODO toast success
-				}, function(reason) {
-					alert("Failed: " + reason);
-					//TODO toast failed
-				});
-			});
-		};
+    $scope.hasImage = function () {
+      return $scope.map.image;
+    };
 
-		$scope.hasImage = function() {
-			return $scope.map.image;
-		};
+    $scope.getImageUrl = function () {
+      if (!$scope.map.image) {
+        return "/images/map.png";
+      }
+      return "/api/files/" + $scope.map.image;
+    };
 
-		$scope.getImageUrl = function() {
-			if (!$scope.map.image) {
-				return "/images/map.png";
-			}
-			return "/api/files/" + $scope.map.image;
-		};
-
-		$scope.$watch("file", function() {
-			if (!$scope.file) {
-				return;
-			}
-			var promise = ImageService.upload($scope.file[0]);
-			promise.then(function(sha1) {
-				$scope.map.image = sha1;
-			}, function(reason) {
-				alert("Failed: " + reason);
-				//TODO toast failed
-			});
-		});
-	});
+    // Listeners
+    $scope.$watch("file", function () {
+      if (!$scope.file) {
+        return;
+      }
+      var promise = ImageService.upload($scope.file[0]);
+      promise.then(function (sha1) {
+        $scope.map.image = sha1;
+      }, function (reason) {
+        alert("Failed: " + reason);
+        //TODO toast failed
+      });
+    });
+  });
 }());
