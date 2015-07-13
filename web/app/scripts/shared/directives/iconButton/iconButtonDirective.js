fb09c4f6cf0106312f426b34fb6de758d06083de
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-07-13 21:29:49 +0200

    Made a more general-purpose iconButton directive

---
+++ web/app/scripts/shared/directives/iconButton/iconButtonDirective.js
@@ -0,0 +1,20 @@
+/* global alert */
+/* global angular */
+
+(function() {
+	"use strict";
+	var app = angular.module("main");
+
+	app.directive("iconButton", function() {
+		return {
+			restrict: "E",
+      scope: {
+        icon: "@",
+        tooltip: "@",
+        uiSref: "@"
+      },
+			templateUrl: "/scripts/shared/directives/iconButton/iconButtonView.html"
+		};
+	});
+
+}());
